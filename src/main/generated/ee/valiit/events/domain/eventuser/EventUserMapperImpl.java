c981d686f8537349ac321431375a17ca914b3d39
katrisaar <katrisaar0@gmail.com>
2023-05-26 10:50:04 +0300

    Impl

--- src/main/generated/ee/valiit/events/domain/eventuser/EventUserMapperImpl.java
+++ src/main/generated/ee/valiit/events/domain/eventuser/EventUserMapperImpl.java
@@ -18,7 +18,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-25T11:02:44+0300",
+    date = "2023-05-26T08:46:43+0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.6 (JetBrains s.r.o.)"
 )
 @Component
