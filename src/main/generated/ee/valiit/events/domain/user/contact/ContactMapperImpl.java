a61f2e3342083302ea8e6845692c75a4ad9e0d2f
ilona <ilona.marken@yahoo.com>
2023-06-01 13:37:27 +0300

    koodi korrastamine kuni lõpuni

--- src/main/generated/ee/valiit/events/domain/user/contact/ContactMapperImpl.java
+++ src/main/generated/ee/valiit/events/domain/user/contact/ContactMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-23T16:11:28+0300",
+    date = "2023-06-01T11:32:48+0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.6 (Eclipse Adoptium)"
 )
 @Component
